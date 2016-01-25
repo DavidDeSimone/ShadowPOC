fa3cd5bac6c06ed52befa4a2cae18d5a93034c6c
David <david@davids-air.attlocal.net>
2016-01-24 21:48:25 -0800

    Initial Commit

---
+++ ShadowPOC/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ShadowPOC
+//
+//  Created by David on 1/24/16.
+//  Copyright © 2016 David. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
